d641de0f5b6bb3f5342295b4c6222860414d261f
Flopster101 <nahuelgomez329@gmail.com>
2023-02-15 01:11:46 -0300

    TinyCK Alpha
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=CaraKernel Milestone 4 for Ginkgo/Willow by @Flopster101
+kernel.string=Tiny CaraKernel ALPHA for Ginkgo/Willow by @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=1
