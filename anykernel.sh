223cd3d62238cea18b5b41985040d000b6e04116
Joel Gómez <nahuelgomez329@gmail.com>
2024-09-01 02:14:11 -0300

    Milestone 20 for Nethunter!
    Hm.
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=CaraKernel Nethunter MS19 for Ginkgo | @Flopster101
+kernel.string=CaraKernel Nethunter MS20 for Ginkgo | @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=1
